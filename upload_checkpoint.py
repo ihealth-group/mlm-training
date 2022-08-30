b1176e33f33325a0a3f511ec2a4657777e98e154
Rafael Morais <rdemorais.freitas@gmail.com>
2022-08-30 15:32:37 -0300

    helper to upload files to s3

--- upload_checkpoint.py
+++ upload_checkpoint.py
@@ -10,4 +10,4 @@ for root, dirs, files in os.walk(CN_MODEL_NAME):
     local_path = os.path.join(root, filename)
     relative_path = os.path.relpath(local_path, CN_MODEL_NAME)
     s3_path = os.path.join('shc-lm-v3', relative_path)
-    s3.upload_file(filename, 'shc-ai-models', s3_path)
+    s3.upload_file(local_path, 'shc-ai-models', s3_path)
