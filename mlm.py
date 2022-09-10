3ba3d5eaac335a886152803490c1073477438c0c
Rafael Morais <rdemorais.freitas@gmail.com>
2022-09-09 23:00:23 -0300

    roberta base custom tokenizer

--- mlm.py
+++ mlm.py
@@ -13,7 +13,7 @@ from transformers import (
   TrainingArguments
 )
 
-CN_MODEL_NAME = 'shc-lm-v3-XLMRobertaL'
+CN_MODEL_NAME = 'shc-lm-v3-XLMRobertaB'
 CORPUS_BUCKET = 'shc-mlm-corpus'
 CORPUS_TRAIN = 'corpus.shc'
 CORPUS_DEV = 'corpus_dev.shc'
