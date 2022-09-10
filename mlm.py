4dfb1c613c7af9e0aa5819a8c76f96381be67ef6
Rafael Morais <rdemorais.freitas@gmail.com>
2022-09-10 09:00:30 -0300

    pre-training existing model

--- mlm.py
+++ mlm.py
@@ -1,7 +1,10 @@
 from transformers import set_seed
 from datasets import load_dataset
+from pathlib import Path
+import tarfile
 import boto3
 import wandb
+import tqdm
 import os
 
 from transformers import (
@@ -13,11 +16,12 @@ from transformers import (
   TrainingArguments
 )
 
-CN_MODEL_NAME = 'shc-lm-v3-2xV100'
+CN_MODEL_NAME = 'shc-lm-v3.1'
 CORPUS_BUCKET = 'shc-mlm-corpus'
+ROOT_BUCKET = 'shc-ai-models'
 CORPUS_TRAIN = 'corpus.shc'
 CORPUS_DEV = 'corpus_dev.shc'
-BERT_MODEL_NAME = 'xlm-roberta-base'
+BERT_MODEL_NAME = Path('shc-lm-v3')
 PROJECT_NAME = 'shc-lm'
 TOKENIZER_DIR = 'shc_cn_tokenizer_bpe_52k'
 
@@ -26,16 +30,38 @@ wandb.login()
 w_run = wandb.init(project=PROJECT_NAME, notes="LM Model from XLM Roberta Base")
 
 s3 = boto3.client('s3')
+
+if not BERT_MODEL_NAME.exists():
+  kwargs = {"Bucket": 'shc-ai-models', "Key": BERT_MODEL_NAME}
+  object_size = s3.head_object(**kwargs)["ContentLength"]
+  with tqdm.tqdm(total=object_size, unit="B", unit_scale=True, desc=BERT_MODEL_NAME) as pbar:
+    s3.download_file(
+      ROOT_BUCKET,
+      f'language_model/{BERT_MODEL_NAME}.tar.gz',
+      f'{BERT_MODEL_NAME}.tar.gz',
+      Callback=lambda bytes_transferred: pbar.update(bytes_transferred)
+    )
+
+    tar = tarfile.open(f'{str(BERT_MODEL_NAME)}.tar.gz')
+    tar.extractall(Path('assets'))
+    tar.close()
+
 if not os.path.exists(CORPUS_TRAIN):
-  s3.download_file(CORPUS_BUCKET, CORPUS_TRAIN, CORPUS_TRAIN)
-# if not os.path.exists(CORPUS_DEV):
-#   s3.download_file(CORPUS_BUCKET, CORPUS_DEV, CORPUS_DEV)
+  kwargs = {"Bucket": CORPUS_BUCKET, "Key": CORPUS_TRAIN}
+  object_size = s3.head_object(**kwargs)["ContentLength"]
+  with tqdm.tqdm(total=object_size, unit="B", unit_scale=True, desc=CORPUS_TRAIN) as pbar:
+    s3.download_file(
+      CORPUS_BUCKET,
+      CORPUS_TRAIN,
+      CORPUS_TRAIN,
+      Callback=lambda bytes_transferred: pbar.update(bytes_transferred)
+    )
 
 training_args = TrainingArguments(
   output_dir=f'./{CN_MODEL_NAME}',
   overwrite_output_dir=True,
-  num_train_epochs=2,
-  per_device_train_batch_size=8,
+  num_train_epochs=1,
+  per_device_train_batch_size=16,
   gradient_accumulation_steps=2,
   gradient_checkpointing=True,
   optim="adafactor",
@@ -54,10 +80,8 @@ set_seed(42)
 
 trains_ds = load_dataset('text', data_files={'train': [CORPUS_TRAIN]})
 
-# vals_ds = load_dataset('text', data_files={'test': [CORPUS_DEV]})
-
 config = AutoConfig.from_pretrained(BERT_MODEL_NAME)
-tokenizer = RobertaTokenizerFast.from_pretrained(TOKENIZER_DIR, max_len=512, add_prefix_space=True)
+tokenizer = RobertaTokenizerFast.from_pretrained(BERT_MODEL_NAME, max_len=512, add_prefix_space=True)
 
 model = AutoModelForMaskedLM.from_pretrained(
   BERT_MODEL_NAME,
@@ -70,18 +94,10 @@ def tokenize_function(examples):
     examples["text"],
     padding=True,
     truncation=True,
-    max_length=510,
+    max_length=512,
     return_special_tokens_mask=True
   )
 
-
-# tokenized_ds_eval = vals_ds['test'].map(
-#   tokenize_function,
-#   batched=True,
-#   num_proc=1,
-#   remove_columns=['text'],
-#   load_from_cache_file=True,
-# )
 
 tokenized_ds_train = trains_ds['train'].map(
   tokenize_function,
